f8fff43cc3a3558e2cdce052a4a1f77e2cdb6081
mrmxf <bruce@mrmxf.com>
2025-02-20 10:11:04 +0000

    fix clog Check + log inline colors

--- clog-sample.go
+++ clog-sample.go
@@ -50,6 +50,5 @@ func init() {
 	_, file, _, _ := runtime.Caller(0)
 	slog.Debug("init " + file)
 
-	slogger.UsePrettyLogger(slog.LevelError)
-	// slogger.UsePrettyLogger(slog.LevelDebug)
+	slogger.UsePrettyLogger(slog.LevelInfo)
 }
